36c84c1d484b9a4decbd2c592f8272b92d3f3750
Krithika Raghavan <krithika.raghavan@gigzolo.com>
2014-02-10 13:35:00 -0500

    Basic home page

--- twilioapp.js
+++ twilioapp.js
@@ -1,15 +1,25 @@
 var express = require('express');
-var twilioClient = require('twilio')
+var twilioClient = require('twilio');
+var http = require('http');
 var accountSid = 'AC93f2920dcda1cf4e3048eb95d3dbb6cc'; 
 var authToken = 'f0f83955e63cffc7d451e0ed3d5c1834'; 
  
+
 var app	= express();
+// Create the server
+var server = http.createServer(app);
 //require the Twilio module and create a REST client 
 var restClient = twilioClient(accountSid, authToken);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
-app.get('/stooges/*?', function(req,res){
-	res.render('stooges', {stooge: null});
-}); 
+//app.post('/call', function(req,res){
+//	res.render('stooges', {stooge: null});
+//}); 
+
+app.get('/', function(req, res){
+	res.render('home');
+});
 var port = 8010;
 app.listen(port);
 console.log('listening on port: '+ port);
